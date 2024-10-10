e06ef8c003ec87041d42a6347b7b11145cfbaaa8
André Nascimento <Andre.Nascimento.Contractor@pepsico.com>
2024-10-10 09:59:51 -0300

    Teste de Exceções - Final Item 5

--- src/main/java/dev/andrenascimento/biblioteca/services/EmprestimoService.java
+++ src/main/java/dev/andrenascimento/biblioteca/services/EmprestimoService.java
@@ -10,6 +10,13 @@ import dev.andrenascimento.biblioteca.models.Obra;
 public class EmprestimoService {
 
     public Emprestimo novo(Cliente cliente, List<Obra> obras) {
+        if (cliente == null){
+            throw new IllegalArgumentException("Cliente não pode ser nulo");
+        }
+
+        if(obras == null || obras.isEmpty()){
+            throw new IllegalArgumentException("Obras não podem ser nulas e nem vazias");
+        }
         var emprestimo = new Emprestimo();
 
         var dataEmprestimo = LocalDate.now();
--- src/test/java/dev/andrenascimento/biblioteca/services/EmprestimoServiceTest.java
+++ src/test/java/dev/andrenascimento/biblioteca/services/EmprestimoServiceTest.java
@@ -1,8 +1,10 @@
 package dev.andrenascimento.biblioteca.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -34,7 +36,7 @@ public class EmprestimoServiceTest {
 
     @Test
     void quandoMetodoNovoForChamadoComClienteDeReputacaoRuimDeveRetornarUmEmprestimoComDevolucaoParaUmDias() {
-        // cenario
+        // cenário
         var emprestimoService = new EmprestimoService();
         var cliente = new Cliente(1L, "Cliente Teste", LocalDate.now(), "123.123.123-12", Reputacao.RUIM);
         var autor = new Autor(1l, "Autor Teste", LocalDate.now(), null);
@@ -75,5 +77,49 @@ public class EmprestimoServiceTest {
 
         // verificação
         assertEquals(LocalDate.now().plusDays(5), emprestimo.getDataDevolucao());
+    }
+
+    @Test
+    void quandoMetodoNovoForChamadoComObraNulaDeveLancarUmaExcecaoDoTipoIllegalArgumentException() {
+        // cenário
+        var emprestimoService = new EmprestimoService();
+        var cliente = new Cliente(1L, "Cliente Teste", LocalDate.now(), "123.123.123-12", Reputacao.REGULAR);
+        var mensagemEsperada = "Obras não podem ser nulas e nem vazias";
+
+        // execução
+        var exception = assertThrows(IllegalArgumentException.class, () -> emprestimoService.novo(cliente, null));
+
+        // verificação
+        assertEquals(mensagemEsperada, exception.getMessage());
+    }
+
+    @Test
+    void quandoMetodoNovoForChamadoComObraVaziaDeveLancarUmaExcecaoDoTipoIllegalArgumentException() {
+        // cenário
+        var emprestimoService = new EmprestimoService();
+        var cliente = new Cliente(1L, "Cliente Teste", LocalDate.now(), "123.123.123-12", Reputacao.REGULAR);
+        var obras = new ArrayList<Obra>();
+        var mensagemEsperada = "Obras não podem ser nulas e nem vazias";
+
+        // execução
+        var exception = assertThrows(IllegalArgumentException.class, () -> emprestimoService.novo(cliente, obras));
+
+        // verificação
+        assertEquals(mensagemEsperada, exception.getMessage());
+    }
+
+    @Test
+    void quandoMetodoNovoForChamadoComClienteNuloDeveLancarUmaExcecaoDoTipoIllegalArgumentException() {
+        // cenário
+        var emprestimoService = new EmprestimoService();
+        var autor = new Autor(1l, "Autor Teste", LocalDate.now(), null);
+        var obra = new Obra(1L, "Obra Teste", 100, Tipo.LIVRO, autor);
+        var mensagemEsperada = "Cliente não pode ser nulo";
+
+        // execução
+        var exception = assertThrows(IllegalArgumentException.class, () -> emprestimoService.novo(null, List.of(obra)));
+
+        // verificação
+        assertEquals(mensagemEsperada, exception.getMessage());
     }
 }
